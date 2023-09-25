2b6d00b7c532b1fbf154a474c2300b51041b8abb
alexeicaps <caps.alex@mail.ru>
2023-09-25 21:31:43 +0300

    Update Task_2 and Task_3

--- Task_2.py
+++ Task_2.py
@@ -3,3 +3,17 @@
 # list_1 = [1, 2, 3, 4, 5]
 # k = 6
 # # 5
+
+# data = [int(input(f'Enter {i + 1} element: ')) for i in range(int(input('Enter array size: ')))]
+# x = int(input('Enter comparison value '))
+list_1 = [2, 16, 9, 32, 25]
+k = 12
+number = list_1[0]
+diff = k - number
+for i in list_1:
+    if abs(k - i) < diff:
+        diff = k - i
+        if diff < 0:
+            diff = diff * (-1)
+        number = i
+print(number)
--- Task_3.py
+++ Task_3.py
@@ -15,3 +15,74 @@
 # Й, Ы – 4 очка;
 # Ж, З, Х, Ц, Ч – 5 очков;
 
+k = 'Ноутбук'
+our_dict = {'A, E, I, O, U, L, N, S, T, R, А, В, Е, Ё, И, Й, Н, О, Р, С, Т': 1,
+            'D, G, Д, К, Л, М, П, У': 2,
+            'B, C, M, P, Б, Г, Ё, Ь, Ъ, Я': 3,
+            'F, H, V, W, Y, Й, Ш, Щ, Ы': 4,
+            'K, Ж, З, Х, Ц, Ч, Э, Ю': 5,
+            'J, X, Ф': 8,
+            'Q, Z': 10}
+
+a = k.upper()
+# print(a)
+# word = input("Type a word: ").upper()
+result = 0
+for char in a:
+    for key in our_dict:
+        if char in key:
+            result += our_dict[key]
+print(result)
+
+
+
+
+
+
+
+
+
+# dictionary = {1: 'A, E, I, O, U, L, N, S, T, R, А, В, Е, И, Н, О, Р, С, Т',
+#               2: 'D, G, Д, К, Л, М, П, У',
+#               3: 'B, C, M, P, Б, Г, Ё, Ь, Я',
+#               4: 'F, H, V, W, Y, Й, Ы',
+#               5: 'K, Ж, З, Х, Ц, Ч',
+#               8: 'J, X',
+#               10: 'Q, Z, Ф, Щ, Ъ'}
+# word = str(input('Enter a word: ').upper())
+#
+# score = 0
+# for char in word:
+#     for key in dictionary:
+#         if char in list(dictionary[key]):
+#             score += key
+#
+# print(score)
+
+# d = {
+#     1 : 'one',
+#     2 : 'two',
+#     3 : 'three'
+# }
+# d[4] = 'four'
+# print(d)
+# d[5] = 'five'
+# print(d)
+# d[3] = 'три'
+# print(d)
+
+# person = {}
+# s = 'john alen york sgu 1 2 3 4 5 6'
+# s = s.split()
+# person['lastname'] = s[0]
+# print(person)
+
+# d = {
+#     1 : 'one',
+#     2 : 'two',
+#     3 : 'three'
+# }
+# d[4] = 'four'
+# # print(d)
+# for key in d:
+#     print(key,':', d[key])
